1160d845bb3455dbe4e7274d5ab3bc76c91e7f79
jyoti1998-gehlot <74006741+jyoti1998-gehlot@users.noreply.github.com>
2021-03-25 22:50:10 +0530

    updated commit

--- get.tf
+++ get.tf
@@ -1,28 +1,37 @@
-resource "aws_api_gateway_method" "process_payload_method_get" {
-  rest_api_id      = aws_api_gateway_rest_api.apiGateway.id
-  resource_id      = aws_api_gateway_resource.process_payload.id
-  http_method      = "GET"
-  authorization    = "AWS_IAM"
+resource "aws_api_gateway_resource" "healthcheck" {
+  parent_id   = aws_api_gateway_rest_api.panda.root_resource_id
+  path_part   = "healthcheck"
+  rest_api_id = aws_api_gateway_rest_api.panda.id
 }
 
-resource "aws_api_gateway_integration" "integration" {
-   rest_api_id              = aws_api_gateway_rest_api.apiGateway.id
-   resource_id              = aws_api_gateway_resource.process_payload.id
-   http_method              = aws_api_gateway_method.process_payload_method_get.http_method
-   integration_http_method  = "GET"
-   type                     = "MOCK"
-   uri                      = var.lambda_invoke_arn
+resource "aws_api_gateway_method" "panda1" {
+  rest_api_id          = aws_api_gateway_rest_api.panda.id
+  resource_id          = aws_api_gateway_resource.healthcheck.id
+  http_method          = "GET"
+  authorization        = "NONE"
+}
 
-   request_parameters = {
+
+resource "aws_api_gateway_integration" "integration" {
+  
+  rest_api_id             = aws_api_gateway_rest_api.panda.id
+  resource_id             = aws_api_gateway_resource.healthcheck.id
+  http_method             = aws_api_gateway_method.panda1.http_method
+  type                    = "MOCK"
+  
+  request_parameters = {
     "integration.request.header.X-Authorization" = "'static'"
   }
 
+  # Transforms the incoming XML request to JSON
   request_templates = {
     "application/xml" = <<EOF
 {
+   
    "statusCode" : 200,
-   "message" : "Healthy" 
+   "message" : "Healthy"
 }
 EOF
   }
 }
+
