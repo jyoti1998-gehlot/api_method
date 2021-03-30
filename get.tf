6c3fb5be45b5427778256110ac914160df37600e
jyoti1998-gehlot <74006741+jyoti1998-gehlot@users.noreply.github.com>
2021-03-30 20:36:55 +0530

    Update get.tf

--- get.tf
+++ get.tf
@@ -1,3 +1,7 @@
+resource "aws_api_gateway_rest_api" "panda" {
+  name = "panda"
+}
+
 resource "aws_api_gateway_resource" "healthcheck" {
   parent_id   = aws_api_gateway_rest_api.panda.root_resource_id
   path_part   = "healthcheck"
@@ -7,9 +11,10 @@ resource "aws_api_gateway_resource" "healthcheck" {
 resource "aws_api_gateway_method" "panda1" {
   rest_api_id          = aws_api_gateway_rest_api.panda.id
   resource_id          = aws_api_gateway_resource.healthcheck.id
-  http_method          = "GET"
+  http_method          = "POST"
   authorization        = "NONE"
 }
+
 
 
 resource "aws_api_gateway_integration" "integration" {
@@ -51,5 +56,53 @@ resource "aws_api_gateway_deployment" "panda1" {
     create_before_destroy = true
   }
 }
+
+resource "aws_api_gateway_resource" "panda" {
+  parent_id   = aws_api_gateway_rest_api.panda.root_resource_id
+  path_part   = "panda"
+  rest_api_id = aws_api_gateway_rest_api.panda.id
+}
+
+resource "aws_api_gateway_method" "panda" {
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.panda.id
+  rest_api_id   = aws_api_gateway_rest_api.panda.id
+}
+
+resource "aws_api_gateway_integration" "panda" {
+  http_method = aws_api_gateway_method.panda.http_method
+  resource_id = aws_api_gateway_resource.panda.id
+  rest_api_id = aws_api_gateway_rest_api.panda.id
+  type        = "MOCK"
+}
+
+
+
+resource "aws_api_gateway_deployment" "panda" {
+  rest_api_id = aws_api_gateway_rest_api.panda.id
+
+  triggers = {
+   
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.panda.id,
+      aws_api_gateway_method.panda.id,
+      aws_api_gateway_integration.panda.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "panda" {
+  depends_on = [aws_api_gateway_deployment.panda1, aws_api_gateway_stage.panda]
+  deployment_id = aws_api_gateway_deployment.panda.id
+  rest_api_id   = aws_api_gateway_rest_api.panda.id
+  stage_name    = "panda"
+}
+
+
 
 
