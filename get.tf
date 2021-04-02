87f2b489d2703f6a24f1d34366e51800d2e1f8b4
jyoti1998-gehlot <74006741+jyoti1998-gehlot@users.noreply.github.com>
2021-04-02 20:10:10 +0530

    Update get.tf

--- get.tf
+++ get.tf
@@ -52,16 +52,6 @@ resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
   http_method = aws_api_gateway_method.panda1.http_method
   status_code = aws_api_gateway_method_response.response_200.status_code
 
-  # Transforms the backend JSON response to XML
-  response_templates = {
-    "application/xml" = <<EOF
-#set($inputRoot = $input.path('$'))
-<?xml version="1.0" encoding="UTF-8"?>
-<message>
-    $inputRoot.body
-</message>
-EOF
-  }
 }
 resource "aws_api_gateway_deployment" "panda1" {
   rest_api_id = aws_api_gateway_rest_api.panda.id
