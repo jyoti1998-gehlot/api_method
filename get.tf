9d99c8ff8a8e8caadabb0adde7ae5a79e08af404
jyoti1998-gehlot <74006741+jyoti1998-gehlot@users.noreply.github.com>
2021-04-02 18:18:24 +0530

    Update get.tf

--- get.tf
+++ get.tf
@@ -39,7 +39,30 @@ resource "aws_api_gateway_integration" "integration" {
 EOF
   }
 }
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.panda.id
+  resource_id = aws_api_gateway_resource.healthcheck.id
+  http_method = aws_api_gateway_method.panda1.http_method
+  status_code = "200"
+}
 
+resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.panda.id
+  resource_id = aws_api_gateway_resource.healthcheck.id
+  http_method = aws_api_gateway_method.panda1.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+
+  # Transforms the backend JSON response to XML
+  response_templates = {
+    "application/xml" = <<EOF
+#set($inputRoot = $input.path('$'))
+<?xml version="1.0" encoding="UTF-8"?>
+<message>
+    $inputRoot.body
+</message>
+EOF
+  }
+}
 resource "aws_api_gateway_deployment" "panda1" {
   rest_api_id = aws_api_gateway_rest_api.panda.id
 
@@ -102,7 +125,3 @@ resource "aws_api_gateway_stage" "panda" {
   rest_api_id   = aws_api_gateway_rest_api.panda.id
   stage_name    = "panda"
 }
-
-
-
-
